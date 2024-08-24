555171bc9bcbceb2ada1ba0d6531864965e9af18
yunweneric <yunweneric@gmail.com>
2024-08-24 22:14:24 +0100

    updating loading theming

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -1,6 +1,26 @@
-let currentRotation = 0;
+// Detects the user's preferred color scheme
+const userPrefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+const cube =document.getElementById("cube");
+// Apply the dark mode class if the user prefers dark mode
+if (userPrefersDark) {
+  document.body.classList.add('dark-mode');
+  cube.classList.add('dark-mode');
+}
+
+// Listen for changes to the user's color scheme preference
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (e) => {
+  if (e.matches) {
+    document.body.classList.add('dark-mode');
+    cube.classList.add('dark-mode');
+} else {
+    document.body.classList.remove('dark-mode');
+    cube.classList.remove('dark-mode');
+  }
+});
+
+let currentRotation = 90;
 const rotateCube = () => {
-    const cube = document.getElementById("cube");
     currentRotation += 90;
     cube.style.transform = `rotateX(${currentRotation}deg)`;
     console.log("currentRotation", currentRotation)
@@ -13,21 +33,20 @@ const initialize = () => {
     _flutter.loader.load({
         onEntrypointLoaded: async function (engineInitializer) {
             const appRunner = await engineInitializer.initializeEngine();
-
-            if (currentRotation > 360) {
-                console.log("App Loaded!", currentRotation)
-                await appRunner.runApp();
-                clearInterval(interval);
-                hidePreloader();
-            } else {
-                const timeout = (360 / currentRotation) * 5000;
-                console.log("App not Loaded!, addding timeout of ", timeout)
-                setTimeout(async () => {
-                    await appRunner.runApp();
-                    clearInterval(interval);
-                    hidePreloader();
-                }, timeout);
-            }
+            // if (currentRotation > 360) {
+            //     console.log("App Loaded!", currentRotation)
+            //     await appRunner.runApp();
+            //     clearInterval(interval);
+            //     hidePreloader();
+            // } else {
+            //     const timeout = (360 / currentRotation) * 5000;
+            //     console.log("App not Loaded!, addding timeout of ", timeout,"currentRotation",currentRotation)
+            //     setTimeout(async () => {
+            //         await appRunner.runApp();
+            //         clearInterval(interval);
+            //         hidePreloader();
+            //     }, timeout);
+            // }
         }
     });
 }
