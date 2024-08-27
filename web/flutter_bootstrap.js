ad8b4717c9932a39c3e1c086a5522df94e1f2990
yunweneric <yunweneric@gmail.com>
2024-08-27 05:48:19 +0100

    deployed new preloader

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -1,13 +1,19 @@
 
+var appRunner;
+const button = document.getElementById("skip_btn")
 
+button.addEventListener(("click"),async()=>{
+    await appRunner.runApp();
+})
 
 const initialize = () => {
     {{flutter_js}}
     {{flutter_build_config}}
     _flutter.loader.load({
         onEntrypointLoaded: async function (engineInitializer) {
-            const appRunner = await engineInitializer.initializeEngine();           
-                const timeout = 10000;
+             appRunner = await engineInitializer.initializeEngine();           
+                const timeout = 50000;
+                button.classList.add('active')
                 console.log("App not Loaded!, adding timeout of ", timeout, "timeout", timeout)
                 setTimeout(async () => {
                     await appRunner.runApp();
--- web/loader.js
+++ web/loader.js
@@ -1,63 +1,68 @@
-
-
+const lightLogo = document.querySelector(".lightLogo");
+const darkLogo = document.querySelector(".darkLogo");
 
 document.addEventListener("DOMContentLoaded", () => {
+  console.log("DOMContentLoaded");
+  // Detects the user's preferred color scheme
+  const userPrefersDark =
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches;
 
+  // Apply the dark mode class if the user prefers dark mode
+  if (userPrefersDark) {
+    document.body.classList.add("dark-mode");
+    darkLogo.classList.add("showDarkLogo");
+  } else {
+    lightLogo.classList.add("showLightLogo");
+  }
 
-  console.log("DOMContentLoaded")
-    // Detects the user's preferred color scheme
-    const userPrefersDark =
-      window.matchMedia &&
-      window.matchMedia("(prefers-color-scheme: dark)").matches;
+  // Listen for changes to the user's color scheme preference
+  window
+    .matchMedia("(prefers-color-scheme: dark)")
+    .addEventListener("change", (e) => {
+      if (e.matches) {
+        document.body.classList.add("dark-mode");
+        darkLogo.classList.add("showDarkLogo");
+        lightLogo.classList.remove("showLightLogo");
+      } else {
+        document.body.classList.remove("dark-mode");
+        darkLogo.classList.remove("showDarkLogo");
+        lightLogo.classList.add("showLightLogo");
+      }
+    });
+  const textElement = document.querySelector(".animated-text");
+  const strings = [
+    "Hi!",
+    "Welcome to Flutter Widget Hub!",
+    "Enjoy Our Vast Collection of Flutter Templates, Blocks, Animations and Effects",
+    "We are getting everything ready for you!",
+    "Initial Load time might be longer than expected, Please stay put!",
+  ];
+  let index = 0;
 
-    // Apply the dark mode class if the user prefers dark mode
-    if (userPrefersDark) {
-      document.body.classList.add("dark-mode");
-    }
+  function animateText() {
+    // Remove 'active' class to initiate fade out and zoom out
+    textElement.classList.remove("active");
+    textElement.classList.add("hidden");
 
-    // Listen for changes to the user's color scheme preference
-    window
-      .matchMedia("(prefers-color-scheme: dark)")
-      .addEventListener("change", (e) => {
-        if (e.matches) {
-          document.body.classList.add("dark-mode");
-        } else {
-          document.body.classList.remove("dark-mode");
-        }
-      });
-    const textElement = document.querySelector(".animated-text");
-    const strings = [
-      "Hi!",
-      "Welcome to Flutter Widget Hub!",
-      "Enjoy Our Vast Collection of Flutter Templates, Blocks, Animations and Effects",
-      "We are getting everything ready for you!",
-      "Initial Load time might be longer than expected, Please stay put!",
-    ];
-    let index = 0;
+    setTimeout(() => {
+      // After the text fades out, change the text content
+      textElement.textContent = strings[index];
 
-    function animateText() {
-      // Remove 'active' class to initiate fade out and zoom out
-      textElement.classList.remove("active");
-      textElement.classList.add("hidden");
+      // Remove 'hidden' class to reset the scale and opacity before adding 'active'
+      textElement.classList.remove("hidden");
+      textElement.classList.add("active");
 
-      setTimeout(() => {
-        // After the text fades out, change the text content
-        textElement.textContent = strings[index];
+      // Move the current item to the end of the list to create an infinite loop
+      // strings.push(strings.shift());
 
-        // Remove 'hidden' class to reset the scale and opacity before adding 'active'
-        textElement.classList.remove("hidden");
-        textElement.classList.add("active");
+      index = (index + 1) % strings.length;
+    }, 1000); // Wait for fade-out and zoom-out to complete (1 second)
+  }
 
-        // Move the current item to the end of the list to create an infinite loop
-        // strings.push(strings.shift());
+  // Initial call to start the animation
+  animateText();
 
-        index = (index + 1) % strings.length;
-      }, 1000); // Wait for fade-out and zoom-out to complete (1 second)
-    }
-
-    // Initial call to start the animation
-    animateText();
-
-    // Set an interval to animate the text every 2 seconds
-    setInterval(animateText, 5000);
-  });
+  // Set an interval to animate the text every 2 seconds
+  setInterval(animateText, 5000);
+});
