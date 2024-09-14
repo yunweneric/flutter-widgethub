8c9b76e4d09e5c2bf677fab8b44589594c4c3550
yunweneric <yunweneric@gmail.com>
2024-09-14 10:06:49 +0100

    Updating textbase details and removing redundant comments

--- web/script/loader.js
+++
@@ -1,7 +0,0 @@
-
-// Hide preloader once the app is fully loaded
-window.addEventListener('load', () => {
-    const preloader = document.getElementById('preloader');
-    preloader.style.display = 'none';
-    console.log("Page loaded!")
-});
