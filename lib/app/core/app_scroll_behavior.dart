3c92f33b2e058104dd50652d38c9a3dd14a94d2c
yunweneric <yunweneric@gmail.com>
2024-12-10 12:14:35 +0100

    fixing scroll effect on drag devices

--- lib/app/core/app_scroll_behavior.dart
+++ lib/app/core/app_scroll_behavior.dart
@@ -7,5 +7,6 @@ class AppScrollBehavior extends MaterialScrollBehavior {
   Set<PointerDeviceKind> get dragDevices => {
         PointerDeviceKind.touch,
         PointerDeviceKind.mouse,
+        PointerDeviceKind.trackpad,
       };
 }
