39bea2e8287c29ca94269e60c16579d05e2ad72f
yunweneric <yunweneric@gmail.com>
2025-08-22 16:55:24 +0100

    chore: update asset cache with new file versions, reorder Airbnb template components for improved layout consistency, and enhance translation file integration

--- lib/components/presentation/templates/airbnb/airbnb.dart
+++ lib/components/presentation/templates/airbnb/airbnb.dart
@@ -18,8 +18,8 @@ class AirbnbData {
     id: "airbnb-redesign-ui",
     codeComponents: [
       CodeComponent(code: explore1Code, widget: const AirbnbLayout(initialIndex: 0)),
-      CodeComponent(code: explore2Code, widget: const AirbnbLayout(initialIndex: 1)),
       CodeComponent(code: explore3Code, widget: const Explore3()),
+      CodeComponent(code: explore2Code, widget: const AirbnbLayout(initialIndex: 1)),
     ],
     description: LangUtil.trans('airbnbDescription'),
     title: LangUtil.trans('airbnbTitle'),
